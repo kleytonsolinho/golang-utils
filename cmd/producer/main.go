9e1a67d947557ba8e05e452965c9becd06e5f224
Kleyton Solinho <kleytonsolinho@gmail.com>
2024-01-12 17:19:55 -0300

    feat: add events management

---
+++ cmd/producer/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/kleytonsolinho/golang-utils/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "Hello World", "amq.direct")
+}
